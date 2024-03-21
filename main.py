9ef69685260edbd750dff2d46a21638ecd366592
Bryan Alexis <126200318+Br-alexis73@users.noreply.github.com>
2024-03-21 19:23:55 +0000

    best combination of parameters

--- main.py
+++ main.py
@@ -36,6 +36,80 @@ def preprocessing():
     return train_hawk, test_hawk
 
 
+def train_model(x_train, y_train, x_test, y_test, hidden_sizes, loss_function, regularization, learning_rate, epochs, method):
+    input_size = x_train.shape[1]
+    output_size = y_train.shape[1]
+    nn = MultiClassNeuralNetwork(input_size, hidden_sizes, output_size, loss_function, regularization)
+    losses, train_accuracy, test_accuracy = nn.train(x_train, y_train, x_test, y_test, learning_rate, epochs, method)
+    return nn, losses, train_accuracy, test_accuracy
+
+
+def manual_tuning(x_train, y_train, x_test, y_test):
+    hidden_sizes = [8, 5, 7]
+    nn, losses, train_accuracy, test_accuracy = train_model(x_train, y_train, x_test, y_test, hidden_sizes,
+                                                            'cross_entropy', 0.01, 0.01, 100, 'sgd')
+    # Further actions based on the trained model and its performance
+    print("Manual tuning done. Model trained.")
+
+
+def automatic_tuning(x_train, y_train, x_test, y_test):
+    best_accuracy = 0
+    best_params = {}
+    best_losses = None
+    best_train_accuracy = None
+    best_test_accuracy = None
+    loss_functions = ['cross_entropy', 'mse', 'mae']
+    regularizations = [0.01]
+    learning_rates = [0.01, 0.001]
+    epochs = 100
+    methods = ['sgd', 'mini-batch', 'batch']
+    hidden_sizes = [8, 3]
+
+    for loss_function in loss_functions:
+        for regularization in regularizations:
+            for learning_rate in learning_rates:
+                for method in methods:
+                    nn, losses, train_accuracy, test_accuracy = train_model(
+                        x_train, y_train, x_test, y_test, hidden_sizes, loss_function,
+                        regularization, learning_rate, epochs, method)
+
+                    current_accuracy = max(test_accuracy)
+                    if current_accuracy > best_accuracy:
+                        best_accuracy = current_accuracy
+                        best_losses = losses
+                        best_train_accuracy = train_accuracy
+                        best_test_accuracy = test_accuracy
+                        best_params = {
+                            'loss_function': loss_function,
+                            'regularization': regularization,
+                            'learning_rate': learning_rate,
+                            'method': method,
+                            'accuracy': best_accuracy
+                        }
+
+    print(f"Best Parameters: {best_params}")
+    plot_results(best_params, best_losses, best_train_accuracy, best_test_accuracy, epochs)
+    return nn, best_losses, best_train_accuracy, best_test_accuracy
+
+
+def plot_results(params, losses, train_acc, test_acc, epochs):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
+    title_suffix = f"(LR: {params['learning_rate']}, Method: {params['method']}, Loss: {params['loss_function']})"
+
+    ax1.plot(range(epochs), train_acc, label='Training Accuracy')
+    ax1.plot(range(epochs), test_acc, label='Testing Accuracy')
+    ax1.set_title(f'Best Accuracy over Epochs\n{title_suffix}')
+    ax1.set_xlabel('Epoch')
+    ax1.set_ylabel('Accuracy')
+    ax1.legend()
+
+    ax2.plot(range(epochs), losses, label='Training Loss')
+    ax2.set_title(f'Best Loss over Epochs\n{title_suffix}')
+    ax2.set_xlabel('Epoch')
+    ax2.set_ylabel('Loss')
+
+    plt.show()
+
 
 def main():
     train_data, test_data = preprocessing()
@@ -44,92 +118,16 @@ def main():
     print("\nTesting Data:")
     print(test_data.head())
 
-    input_size = train_data.shape[1] - 1
-    output_size = len(np.unique(train_data['Species']))
-    hidden_sizes = [8, 5, 7]
-
     x_train = train_data.drop('Species', axis=1).values
     y_train = pd.get_dummies(train_data['Species']).values
     x_test = test_data.drop('Species', axis=1).values
     y_test = pd.get_dummies(test_data['Species']).values
 
-    loss_functions = ['cross_entropy', 'mse', 'mae']
-    regularizations = [0.01]
-    learning_rates = [0.01, 0.001]
-    epochs = 100
-    methods = ['sgd', 'mini-batch', 'batch']
+    # manual_tuning(x_train, y_train, x_test, y_test)
+    nn, losses, train_accuracy, test_accuracy = automatic_tuning(x_train, y_train, x_test, y_test)
 
-    # Results storage
-    results = []
-
-    for loss_function in loss_functions:
-        for regularization in regularizations:
-            for learning_rate in learning_rates:
-                for method in methods:
-                    # Initialize the neural network with the current set of hyperparameters
-                    nn = MultiClassNeuralNetwork(input_size, hidden_sizes, output_size, loss_function=loss_function,
-                                                 regularization_strength=regularization)
-
-                    # Train the network and get training results
-                    losses, train_accuracy, test_accuracy = nn.train(x_train, y_train, x_test, y_test, learning_rate,
-                                                                     epochs, method=method)
-
-                    # Store hyperparameters and the final accuracy or loss
-                    results.append({
-                        'loss_function': loss_function,
-                        'regularization': regularization,
-                        'learning_rate': learning_rate,
-                        'method': method,
-                        'final_train_accuracy': train_accuracy[-1],
-                        'final_test_accuracy': test_accuracy[-1],
-                        'final_loss': losses[-1]
-                    })
-
-    # Convert results to DataFrame for easy manipulation and plotting
-    results_df = pd.DataFrame(results)
-
-    sorted_results = results_df.sort_values('final_test_accuracy', ascending=False)
-
-    # Select the best result
-    best_result = sorted_results.iloc[0]
-
-    # Initialize the best model with the best hyperparameters
-    best_nn = MultiClassNeuralNetwork(input_size, hidden_sizes, output_size,
-                                      loss_function=best_result['loss_function'],
-                                      regularization_strength=best_result['regularization'])
-
-    # Train the best model to get the epoch-wise accuracies and losses
-    losses, train_accuracy, test_accuracy = best_nn.train(x_train, y_train, x_test, y_test,
-                                                          best_result['learning_rate'], epochs,
-                                                          method=best_result['method'])
-
-    # Now you can plot the epoch-wise accuracies and losses
-    plt.figure(figsize=(14, 6))
-
-    # Plot training and testing accuracies
-    plt.subplot(1, 2, 1)
-    plt.plot(train_accuracy, label='Training Accuracy')
-    plt.plot(test_accuracy, label='Testing Accuracy')
-    plt.title(f'Best Accuracy over Epochs\n(LR: {best_result["learning_rate"]}, '
-              f'Method: {best_result["method"]}, '
-              f'Loss: {best_result["loss_function"]})')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.legend()
-
-    # Plot training loss
-    plt.subplot(1, 2, 2)
-    plt.plot(losses, label='Training Loss')
-    plt.title(f'Best Loss over Epochs\n(LR: {best_result["learning_rate"]}, '
-              f'Method: {best_result["method"]}, '
-              f'Loss: {best_result["loss_function"]})')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.legend()
-
-    plt.tight_layout()
-    plt.show()
-    best_nn.evaluate(x_test, y_test)
+    # After the model is trained, we evaluate it on the test set
+    nn.evaluate(x_test, y_test)
 
 
 if __name__ == "__main__":
